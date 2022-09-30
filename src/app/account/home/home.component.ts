147ff77355376e9214923cf3de651e414cf0ac42
Waleed Khalid <waleed@codexsquared.com>
2022-09-30 18:11:02 +0500

    update assets

--- src/app/account/home/home.component.ts
+++ src/app/account/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  currentUser:any = {
+    profileType : 1
+  };
+  constructor(private service : UserService) { }
 
   ngOnInit(): void {
+    // this.service.currentUser.subscribe((x) => {
+    //   if (x.token !== null) {
+    //     this.currentUser = x;
+    //   }
+    // });
   }
 
 }
