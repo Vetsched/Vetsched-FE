d6810a4b5b6d844f83f7ea851a5f82e36e227d3e
Waleed Khalid <waleed@codexsquared.com>
2022-10-03 00:24:50 +0500

    pet detail popup

--- src/app/account/pet-lovers/add-pet/add-pet.component.ts
+++ src/app/account/pet-lovers/add-pet/add-pet.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core';
 
 @Component({
   selector: 'app-add-pet',
   templateUrl: './add-pet.component.html',
-  styleUrls: ['./add-pet.component.css']
+  styleUrls: ['./add-pet.component.css'],
 })
 export class AddPetComponent implements OnInit {
-
-  constructor() { }
-
+  currentUser: any = {};
+  constructor(private service: UserService) {}
   ngOnInit(): void {
+    this.service.currentUser.subscribe((x) => {
+      if (x.token !== null) {
+        this.currentUser = x;
+      }
+    });
   }
-
 }
