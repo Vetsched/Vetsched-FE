988e3142f026c79f0fbc9fb637477897a8652cdf
Waleed Khalid <waleed@codexsquared.com>
2022-10-24 22:13:50 +0500

    view profile modal

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
-
-  constructor() { }
+  constructor(private service: UserService) {}
 
   ngOnInit(): void {
+    this.service.populate();
   }
-
 }
