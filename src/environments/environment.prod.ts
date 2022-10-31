c385f9c2d33dcc488212661f411b5a2ec7e3c6b5
Waleed Khalid <waleed@codexsquared.com>
2022-10-31 12:55:36 +0500

    update code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  api_url: 'https://vetschedapi.herokuapp.com'
+  api_url: 'https://vetschedapi.herokuapp.com',
+  APP_ID: "223580d7f529d8ea",
+  REGION: "us",
+  AUTH_KEY: "b9dd1cde5160f8bc3545aab3458361432cac0f53",
 };
