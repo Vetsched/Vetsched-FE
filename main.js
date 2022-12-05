efefbc694d823e307ce8b96351539d6b76d39692
Waleed Khalid <waleed@codexsquared.com>
2022-12-05 12:16:43 +0500

    update code

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ app.use(express.static(path.join(__dirname, "build"), { extensions: ["js"] }));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build/index.html"));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(3000);
