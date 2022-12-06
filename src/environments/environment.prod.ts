09bdc540eac707f4297133f1d9677fd7665bfedf
Waleed Khalid <waleed@codexsquared.com>
2022-12-06 20:00:16 +0500

    update code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_url: 'https://vetschedapi.herokuapp.com',
-  APP_ID: "223580d7f529d8ea",
+  APP_ID: "2272103d5e16c18f", // "223580d7f529d8ea",
   REGION: "us",
-  AUTH_KEY: "b9dd1cde5160f8bc3545aab3458361432cac0f53",
+  AUTH_KEY: "d3fc4df7180de96401235997bde747453fb15952", // "b9dd1cde5160f8bc3545aab3458361432cac0f53",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  APP_ID: "223580d7f529d8ea",
+  APP_ID: "2272103d5e16c18f", // "223580d7f529d8ea",
   REGION: "us",
-  AUTH_KEY: "b9dd1cde5160f8bc3545aab3458361432cac0f53",
+  AUTH_KEY: "d3fc4df7180de96401235997bde747453fb15952", // "b9dd1cde5160f8bc3545aab3458361432cac0f53",
   api_url: 'https://vetschedapi.herokuapp.com',
   // api_url: 'https://localhost:7006',
 
